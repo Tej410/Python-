8a8d4f13c788d8da98e868dbb5f6ec690ea5d497
Tej410 <tejdeep20012013@gmail.com>
2024-01-01 22:10:16 -0500

    added constructors

---
+++ constructors.py
@@ -0,0 +1,11 @@
+class Pygram:
+    def __init__(self,name):
+        self.name = name
+
+    def talk(self):
+        print(f"Hi this is {self.name} talking!")
+
+john = Pygram("john")
+print(john.name)
+john.talk()            
+        
